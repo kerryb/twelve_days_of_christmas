2ee453323c8f656cb3c444877e0ebabea40d964a
Kerry Buckley <kerryjbuckley@gmail.com>
2021-11-24 15:55:12 +0000

    Add full stop

--- twelve_days_of_christmas.rb
+++ twelve_days_of_christmas.rb
@@ -1,3 +1,3 @@
 n=["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"]
 l=["twelve drummers drumming, ","eleven pipers piping, ","ten lords a-leaping,\n","nine ladies dancing, ","eight maids a-milking, ","seven swans a-swimming,\n","six geese a-laying, ","five gold rings;\n","four calling birds, ","three French hens, ","two turtle doves\n"]
-puts (0..11).map{|i|"On the #{n[i]} day of Christmas my true love gave to me\n#{l.drop(11-i).join}#{i==0?"":"and "}a partridge in a pear tree\n\n"}.join
+puts (0..11).map{|i|"On the #{n[i]} day of Christmas my true love gave to me\n#{l.drop(11-i).join}#{i==0?"":"and "}a partridge in a pear tree.\n\n"}.join
