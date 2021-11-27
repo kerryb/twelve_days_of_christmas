5c6c11b66e1a4ddaea236307bd630279672908ee
Kerry Buckley <kerryjbuckley@gmail.com>
2021-11-27 16:04:45 +0000

    [475 chars] Concatenate two strings with +

--- twelve_days_of_christmas.rb
+++ twelve_days_of_christmas.rb
@@ -3,5 +3,5 @@ puts (0..11).map{|i|"On the #{%w[first second third fourth fifth sixth seventh e
 |nine ladies dancing, |eight maids a-milking, |seven swans a-swimming,
 |six geese a-laying, |five gold rings;
 |four calling birds, |three French hens, |two turtle doves
-".split(?|)[11-i..]*""}#{"and "if i>0}a partridge in a pear tree.
+".split(?|)[11-i..]*""+"and "if i>0}a partridge in a pear tree.
 "}*?\n
