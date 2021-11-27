6b6a9a7f118608f0bf9c54250b3a20b3e41549e8
Kerry Buckley <kerryjbuckley@gmail.com>
2021-11-27 16:39:22 +0000

    [468 chars] Do the gsub on the whole final string

--- twelve_days_of_christmas.rb
+++ twelve_days_of_christmas.rb
@@ -1,7 +1,7 @@
-puts (0..11).map{|i|"On the #{%w[first second third four fif six seven eigh nin ten eleven twelf][i]}#{"th"if i>2} day of Christmas my true love gave to me
-#{("twelve drummers drummX, |eleven pipers pipX, |ten lords a-leapX,
+puts ((0..11).map{|i|"On the #{%w[first second third four fif six seven eigh nin ten eleven twelf][i]}#{"th"if i>2} day of Christmas my true love gave to me
+#{"twelve drummers drummX, |eleven pipers pipX, |ten lords a-leapX,
 |nine ladies dancX, |eight maids a-milkX, |seven swans a-swimmX,
 |six geese a-layX, |five gold rXs;
 |four callX birds, |three French hens, |two turtle doves
-".split(?|)[11-i..]*"").gsub(?X,"ing")+"and "if i>0}a partridge in a pear tree.
-"}*?\n
+".split(?|)[11-i..]*""+"and "if i>0}a partridge in a pear tree.
+"}*?\n).gsub ?X,"ing"
