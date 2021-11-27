0557bbc2df3dbec2a7303132be812aca9772237a
Kerry Buckley <kerryjbuckley@gmail.com>
2021-11-27 15:52:31 +0000

    [479 chars] Replace drop(n) with [n..]

--- twelve_days_of_christmas.rb
+++ twelve_days_of_christmas.rb
@@ -3,5 +3,5 @@ puts (0..11).map{|i|"On the #{%w[first second third fourth fifth sixth seventh e
 |nine ladies dancing, |eight maids a-milking, |seven swans a-swimming,
 |six geese a-laying, |five gold rings;
 |four calling birds, |three French hens, |two turtle doves
-".split(?|).drop(11-i)*""}#{i==0?"":"and "}a partridge in a pear tree.
+".split(?|)[11-i..]*""}#{i==0?"":"and "}a partridge in a pear tree.
 "}*?\n
