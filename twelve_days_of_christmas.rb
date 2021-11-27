5936a6c4ec2a85797297146a58d97bf663dc8037
Kerry Buckley <kerryjbuckley@gmail.com>
2021-11-27 16:13:35 +0000

    [470 chars] 'th' shennanigans

--- twelve_days_of_christmas.rb
+++ twelve_days_of_christmas.rb
@@ -1,4 +1,4 @@
-puts (0..11).map{|i|"On the #{%w[first second third fourth fifth sixth seventh eighth ninth tenth eleventh twelfth][i]} day of Christmas my true love gave to me
+puts (0..11).map{|i|"On the #{%w[first second third four fif six seven eigh nin ten eleven twelf][i]}#{"th"if i>2} day of Christmas my true love gave to me
 #{"twelve drummers drumming, |eleven pipers piping, |ten lords a-leaping,
 |nine ladies dancing, |eight maids a-milking, |seven swans a-swimming,
 |six geese a-laying, |five gold rings;
