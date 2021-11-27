3752fa06f1e3a676b1b7fec080d761e3734e6841
Kerry Buckley <kerryjbuckley@gmail.com>
2021-11-27 16:24:33 +0000

    [469 chars] Shave of a byte by compressing 'ing'

--- twelve_days_of_christmas.rb
+++ twelve_days_of_christmas.rb
@@ -1,7 +1,7 @@
 puts (0..11).map{|i|"On the #{%w[first second third four fif six seven eigh nin ten eleven twelf][i]}#{"th"if i>2} day of Christmas my true love gave to me
-#{"twelve drummers drumming, |eleven pipers piping, |ten lords a-leaping,
-|nine ladies dancing, |eight maids a-milking, |seven swans a-swimming,
-|six geese a-laying, |five gold rings;
-|four calling birds, |three French hens, |two turtle doves
-".split(?|)[11-i..]*""+"and "if i>0}a partridge in a pear tree.
+#{("twelve drummers drummX, |eleven pipers pipX, |ten lords a-leapX,
+|nine ladies dancX, |eight maids a-milkX, |seven swans a-swimmX,
+|six geese a-layX, |five gold rXs;
+|four callX birds, |three French hens, |two turtle doves
+".split(?|)[11-i..]*"").gsub(?X,"ing")+"and "if i>0}a partridge in a pear tree.
 "}*?\n
